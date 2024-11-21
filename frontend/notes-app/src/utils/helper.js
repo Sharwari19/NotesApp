43da098d6e221519a02d4d64db4a14a442ce81ab
sharwarigole <sharwarigole@gmail.com>
2024-11-21 16:44:33 +0530

    feat| UI for add note, view note on dashboard, edit note

--- frontend/notes-app/src/utils/helper.js
+++ frontend/notes-app/src/utils/helper.js
@@ -26,4 +26,18 @@ export const validatePassword = (password) => {
     });
 
     return isStrong;
+};
+
+export const getInitials = (name) => {
+    if(!name) return "";
+
+    const words = name.split(" ");
+    let initials = "";
+
+    for(let i = 0; i < Math.min(words.length, 2); i++) {
+
+        initials = initials + words[i][0];
+    }
+
+    return initials.toUpperCase();
 }
