73deae0384cabcb44d64f5af9ad297726518abaa
sharwari <sharwarigole@gmail.com>
2024-11-27 13:00:52 +0530

    feat: create routes for editing notes and retrieving all notes

--- backend/index.js
+++ backend/index.js
@@ -165,6 +165,71 @@ app.post('/add-note', authenticateToken, async (req, res) => {
 
 });
 
+// edit a note
+app.put('/edit-note/:noteId', authenticateToken, async (req, res) => {
+    
+    const notedId = req.params.noteId;
+    const { title, content, tags, isPinned } = req.body;
+    const { user } = req.user;
+
+    if(!title && !content && !tags) {
+        return res
+            .status(400)
+            .json({ error: true, message: 'No changes provided'});
+    }
+
+    try {
+        const note = await Note.findOne({ _id: notedId, userId: user._id});
+
+        if(!note) {
+            return res.status(404).json({ error: true, message: 'Note not found' });
+        }
+
+        if(title) note.title = title;
+        if(content) note.content = content;
+        if(tags) note.tags = tags;
+        if(isPinned) note.isPinned = isPinned;
+
+        await note.save();
+
+        return res.json({
+            error: false,
+            note,
+            message: 'Note updated successfully',
+        });
+
+    } catch (error) {
+        return res.status(500).json({
+            error: true,
+            message: 'Internal Server Error',
+        });
+    }
+
+});
+
+
+// get all notes
+app.get('/get-all-notes', authenticateToken, async (req, res) => {
+    const { user } = req.user;
+
+    try {
+        const notes = await Note.find({ userId: user._id})
+                                .sort({ isPinned: -1 });
+
+        return res.json({
+            error: false,
+            notes,
+            message: 'All notes retrieved successfully',
+        });
+    } catch (error) {
+        return res.status(500).json({ 
+            error: true, 
+            message: 'Internal Server Error' 
+        });
+    }
+
+
+});
 
 app.listen(8000);
 
